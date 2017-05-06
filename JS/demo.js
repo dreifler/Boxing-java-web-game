6ed7f011d3630982ff4e49b818b2e0dfeef05b09
Darren Reifler <dreifler@icloud.com>
2017-05-06 15:24:27 -0700

    Add files via upload

---
+++ JS/demo.js
@@ -0,0 +1,10 @@
+function showDiv(elem){
+	if(elem.value == 'demo'){
+                about.style.display='none';
+                about.style.display='block';
+        }else if(elem.value == 'about'){
+                about.style.display = 'block';
+                demo.style.display = 'none';
+        }
+}
+
