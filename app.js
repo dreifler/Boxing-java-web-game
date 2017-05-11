414525fdf9680edcf8615b32c16b3521f3c4bd89
Darren Reifler <dreifler@icloud.com>
2017-05-10 18:23:13 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+(function(){
+ var app = angular.module('account', []);
+
+ app.controller('GymController', function(){
+   this.gym = player;
+ });
+
+  var player = {
+    name: "Darren Reifler",
+    fighters: 10,
+    description: "This is Big D's Gym.",
+    canModify: true,
+  }
+})();
