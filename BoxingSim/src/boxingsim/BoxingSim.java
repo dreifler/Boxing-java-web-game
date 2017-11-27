f965c58135b1e0ccef6ce4e057b02128b3557784
Darren Reifler <dreifler@icloud.com>
2017-11-26 18:19:09 -0800

    Added Fighter Display
    Added option to display all fighters. Removed ropist style to simplify prototype.

--- BoxingSim/src/boxingsim/BoxingSim.java
+++ BoxingSim/src/boxingsim/BoxingSim.java
@@ -7,6 +7,7 @@ package boxingsim;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -19,14 +20,27 @@ public class BoxingSim {
     private static DataWriter dw = new DataWriter();
     
     public static void main(String[] args) throws IOException{
+        boolean quit = false;
     //Initial boxer creation from data file
         boxerSet = dr.CreateBoxers();
     //write data to files both for output(visible stats) and storage(all stats)
         dw.WriteBoxers(boxerSet);
         
-    //List of options to player to continue
-        printMenu();
-        
+    //Player selects option from menu to continue
+        while(!quit){
+            switch(printMenu()) {
+            case 0: quit = true;
+                break;
+            case 1: boxerSet = dr.CreateBoxers();
+                break;
+            case 2: addBoxer();
+                break;
+            case 3: displayFighters();
+                break;
+            case 4: System.out.println("In development");
+                break;
+        }
+    }
 
     }
     
@@ -34,10 +48,15 @@ public class BoxingSim {
         System.out.println("Enter Number to Select Option:");
         System.out.println("1: Create/Re-create fighters.");
         System.out.println("2: Add a new fighter (manual).");
-        System.out.println("3: Practice fight(manual match-up).");
+        System.out.println("3: Display all fighters");
+        System.out.println("4: Practice fight(manual match-up).");
+        System.out.println("0: Exit");
         System.out.print("Selection: ");
         
-        return scanner.nextInt();
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        
+        return choice;     
     }
     
     public static void addBoxer() {
@@ -48,7 +67,7 @@ public class BoxingSim {
         System.out.print("Enter new boxer name: ");
         name = scanner.nextLine();
         
-        System.out.print("Style: 1-Slugger, 2-Dancer, 3-Balanced");
+        System.out.print("Style(1-Slugger, 2-Dancer, 3-Balanced): ");
         choice = scanner.nextInt();
         scanner.nextLine();
         
@@ -58,8 +77,6 @@ public class BoxingSim {
             case 2: style = style.DANCER;
                     break;
             case 3: style = style.BALANCED;
-                    break;
-            case 4: style = style.ROPIST;
                     break;
             default: style = style.BALANCED;
                     break;
@@ -71,5 +88,27 @@ public class BoxingSim {
             dw.addBoxer(newBoxer);
         else
             System.out.println("Boxer already exists.");
+    }
+    
+    public static void displayFighters() {
+        Iterator it = boxerSet.iterator();
+        Boxer boxer;
+        StringBuilder line = new StringBuilder();
+        
+        System.out.println("\nCurrent Fighters:");
+        System.out.println("===============================");
+        System.out.println(String.format("%-10s%-10s%-4s%-4s%-4s", "Name","Style", "Str",
+                "Spd","Agl"));
+        while(it.hasNext()){
+            boxer = (Boxer)it.next();
+            line.append(String.format("%-10s", boxer.name));
+            line.append(String.format("%-10s", boxer.style));
+            line.append(String.format("%-4s", boxer.GetStr()));
+            line.append(String.format("%-4s", boxer.GetSpd()));
+            line.append(String.format("%-4s", boxer.GetAgl()));
+            System.out.println(line.toString());
+            line.delete(0,100);
+        }
+        System.out.println("===============================\n");
     }
 }
