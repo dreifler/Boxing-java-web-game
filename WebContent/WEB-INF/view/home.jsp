2de502377d73f2999e422c903cfbabba4cb38e35
Darren Reifler <dreifler@icloud.com>
2018-06-17 10:49:28 -0700

    Added Custom Login Page

---
+++ WebContent/WEB-INF/view/home.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Boxing App</title>
+</head>
+
+<body>
+	<h2>The Boxing Game Application</h2>
+	<hr>
+	
+	Welcome to the Boxing App home page!
+</body>
+</html>
--- src/boxing/config/AppSecurityConfig.java
+++ src/boxing/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package boxing.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,12 +16,26 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
 		// add our users for in memory authentication
+		@SuppressWarnings("deprecation")
 		UserBuilder users = User.withDefaultPasswordEncoder();
 		
 		auth.inMemoryAuthentication()
 			.withUser(users.username("john").password("test123").roles("PLAYER"))
 			.withUser(users.username("mary").password("test123").roles("MANAGER"))
 			.withUser(users.username("susan").password("test123").roles("ADMIN"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/LoginPage")
+				.loginProcessingUrl("/authenticateUser")
+				.defaultSuccessUrl("/home.jsp")
+				.permitAll();
 	}
 
 	
