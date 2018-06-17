a8b753b9c17bc5f787d1c7be357217fb4dce4bf0
Darren Reifler <dreifler@icloud.com>
2018-06-17 09:43:08 -0700

    Added Initial Maven Security
    Added initial configuration and login (test) for maven security on application.

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,1 +1,12 @@
-<% response.sendRedirect("boxers/list"); %>
+<html>
+<head>
+	<title>Boxing App</title>
+</head>
+
+<body>
+	<h2>The Boxing Game Application</h2>
+	<hr>
+	
+	Welcome to the Boxing App home page!
+</body>
+</html>
---
+++ src/boxing/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package boxing.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
