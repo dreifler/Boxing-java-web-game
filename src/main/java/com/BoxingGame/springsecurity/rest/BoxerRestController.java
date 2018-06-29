0496e7b29d31ac7dddbff6805d64dec8f81a3b03
Darren Reifler <dreifler@icloud.com>
2018-06-28 19:15:32 -0700

    refactored restController

--- src/main/java/com/BoxingGame/springsecurity/rest/BoxerRestController.java
+++ src/main/java/com/BoxingGame/springsecurity/rest/BoxerRestController.java
@@ -3,6 +3,8 @@ package com.BoxingGame.springsecurity.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +15,23 @@ import com.BoxingGame.springsecurity.entity.Boxer;
 @RequestMapping("/api")
 public class BoxerRestController {
 		
-		// define endpoint for "/boxers" - return list of students
+		private List<Boxer> theBoxers;
 		
-		@GetMapping("/boxers")
-		public List<Boxer> getStudents() {
-				
-			List<Boxer> theBoxers = new ArrayList<>();
+		// define @PostConstruct to load the boxer data
+		@PostConstruct
+		public void loadData( ) {
+			theBoxers = new ArrayList<>();
 			
 			theBoxers.add(new Boxer("Yri", "Patel"));
 			theBoxers.add(new Boxer("Mario", "Rossi"));
 			theBoxers.add(new Boxer("Mary", "Smith"));
-			
+		}
+		
+		// define endpoint for "/boxers" - return list of students
+		
+		@GetMapping("/boxers")
+		public List<Boxer> getStudents() {
+							
 			return theBoxers;
 		}
 }
